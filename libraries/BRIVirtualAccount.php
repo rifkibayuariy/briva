6c2e73b8c8d1f8f9212e9d35de3fe30e36936734
rifkibayuariy <email.rifkibayu@gmail.com>
2023-03-16 15:12:56 +0700

    Upload

---
+++ libraries/BRIVirtualAccount.php
@@ -0,0 +1,11 @@
+<?php
+
+class BRIVirtualAccount
+{
+    const institution_code = 'J104408';
+    const briva_number = 777777;
+
+    private $client_id = 'UZBDMARWsHwGdccWroXZNXX3jV9MLLR0';
+    private $client_secret = 'BagtSiZ4L1gLoGBi';
+    private $api_url = 'https://sandbox.partner.api.bri.co.id/';
+}
